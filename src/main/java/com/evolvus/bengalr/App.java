aa0a4dd2d32fe54c3a1a7fee8c41f36ffed0ac46
kirann-paycorp <kiran.n@paycorp.io>
2023-05-17 11:48:48 +0530

    second changes

--- src/main/java/com/evolvus/bengalr/App.java
+++ src/main/java/com/evolvus/bengalr/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello Wzsdfghjhgfd" );
+       for(int i=0;i<10;i++) {
+    	    System.out.println("hii paycorp");
+       }
     }
 }
