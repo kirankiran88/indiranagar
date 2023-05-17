e8c0de2b20d91e8ce73508b39ffdb4a616479e3d
kirann-paycorp <kiran.n@paycorp.io>
2023-05-17 11:09:14 +0530

    make changes

---
+++ src/main/java/com/evolvus/bengalr/App.java
@@ -0,0 +1,13 @@
+package com.evolvus.bengalr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Wzsdfghjhgfd" );
+    }
+}
